21c467119eaa857ade3715733db06aa0eed68add
mayank6 <chauhanmayank.621@gmail.com>
2020-06-03 18:41:27 +0530

    Spring initialisation

---
+++ src/main/java/springgroup/springlearn/FrstappApplication.java
@@ -0,0 +1,13 @@
+package springgroup.springlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrstappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrstappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springgroup/springlearn/FrstappApplicationTests.java
@@ -0,0 +1,13 @@
+package springgroup.springlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrstappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
